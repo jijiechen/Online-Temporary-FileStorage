2c4eaa41b35b5edaaf415a132908da5ca5449ce7
ciznx chan <ciznx@qq.com>
2011-12-04 01:01:13 +0800

    first commit

---
+++ Destination.aspx
@@ -0,0 +1,103 @@
+﻿<%@ Page Language="VB" AutoEventWireup="false" ValidateRequest="false"  CodeFile="Destination.aspx.vb" Inherits="destination" %>
+<!doctype html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+<title>File Storage</title>
+<style>
+div{margin:10px 0;}
+tr:hover{background-color:#dfdfdf}
+</style>
+</head>
+<body>
+<div id="upload">
+<h1>Add</h1>
+<form action="destination.aspx" method="post" enctype="multipart/form-data"><input type="file" name="file" id="file" accept="*/*" /></form>
+<p></p>
+</div>
+<div id="uploaded">
+<h1>Added</h1>
+<table id="RecentlyFiles" style="border-collapse:collapse;">
+<%=FileListStr%>
+</table>
+</div>
+<script>
+	(function () {
+
+		function out(txt) {
+			document.getElementsByTagName('p')[0].innerHTML = txt;
+		}
+		function sendBinary(str, file) {
+			var formData, boundary, xhr = new XMLHttpRequest();
+
+			xhr.open("POST", "Destination.aspx?ajax=true", true);
+			xhr.setRequestHeader("Content-Length", file.size);
+			out("Uploading...");
+
+			xhr.upload.addEventListener("progress", function (e) {
+				if (e.lengthComputable) {
+					var percentage = Math.round((e.loaded * 100) / e.total);
+					out("Uploading..." + percentage + "%");
+				}
+			}, false);
+			xhr.upload.addEventListener("load", function (e) {
+				out("Done");
+				setTimeout(function () {
+					location.reload();
+				}, 1000);
+			}, false);
+			xhr.upload.addEventListener("error", function (e) {
+				out("Error when uploading");
+			}, false);
+
+//			if (xhr.sendAsBinary) {
+//				boundary = "---------------------------Ajax" + String(Number(new Date)).substr(0, 5) + 'Upload';
+//				xhr.setRequestHeader("Content-Type", "multipart/form-data, boundary=" + boundary);
+//				var requestBody = "--" + boundary + "\r\n" +
+//					'Content-Disposition: form-data; name="file"; filename="' + file.name + '"\r\n' +
+//					"Content-Type: Application/Stream-Otect\r\n\r\n" +
+//					str + "\r\n--" + boundary + "--\r\n";
+//				xhr.sendAsBinary(requestBody);
+//			} else if (typeof FormData != "undefined") {
+				formData = new FormData();
+				formData.append('file', file);
+				xhr.send(formData);
+//			} else {
+//				out('Can not send the file by XmlHttpRequest');
+//			}
+		}
+		function uploadFile(data) {
+			if (data.files && data.files.length) {
+				var file = data.files[0], fr = new FileReader();
+				try {
+					out('Initializing...');
+					fr.readAsBinaryString(file);
+					fr.onload = function () {
+						sendBinary(this.result, file);
+					}
+				} catch (err) {
+					return out("Error when reading file");
+				}
+			}
+		}
+		document.ondragover = function (e) { e.preventDefault() };
+		document.ondrop = function (e) {
+			e.stopPropagation();
+			e.preventDefault();
+			uploadFile(e.dataTransfer);
+		};
+		var isIE = navigator.userAgent.toLowerCase().indexOf('msie') > -1;
+		if (isIE) {
+			out("Try Chrome or Firefox to get a better experience");
+		}
+		document.getElementsByTagName('input')[0].onchange = function () {
+			if (isIE) {
+				document.getElementsByTagName("FORM")[0].submit();
+			} else {
+				uploadFile(this);
+			}
+		};
+	})();
+</script>
+</body>
+</html>
---
+++ Destination.aspx.vb
@@ -0,0 +1,144 @@
+﻿Imports System.IO
+Imports System.Collections.Generic
+
+Partial Class destination
+    Inherits System.Web.UI.Page
+
+    Private Const FilePath As String = "~/files/"
+    Public FileListStr As String = String.Empty
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim files As HttpFileCollection = Request.Files
+        Dim delFie As String = Request.QueryString("del")
+        Dim getFile As String = Request.QueryString("get")
+
+        If (Request.HttpMethod = "POST") AndAlso _
+            (Not (files Is Nothing)) AndAlso _
+            (files.Count > 0) Then
+            SaveRequestedFile()
+            If Request.QueryString("ajax") Is Nothing Then
+                Response.Redirect("Destination.aspx")
+            End If
+        Else
+            If (Not (delFie Is Nothing)) AndAlso delFie.Length > 0 Then
+                DeleteFile(delFie)
+                Response.Redirect("Destination.aspx")
+                Return
+            End If
+            If (Not (getFile Is Nothing)) AndAlso getFile.Length > 0 Then
+                DownloadFile(getFile)
+                Return
+            End If
+        End If
+
+        If (Request.QueryString("ajax") Is Nothing) Then
+            WriteFileList()
+        End If
+
+    End Sub
+
+    Private Sub SaveRequestedFile()
+        Dim files As HttpFileCollection = Request.Files
+
+        Dim fileName As String = Guid.NewGuid().ToString("N")       '取新文件名种子
+        Dim file As HttpPostedFile = files(0)                       '获取要转存的文件
+        Dim fileConfigName As String         '定义新的文件名
+
+        fileConfigName = String.Concat(fileName, ".config")
+
+        file.SaveAs(Server.MapPath(String.Concat(FilePath, fileName)))
+        Dim configFile As StreamWriter = System.IO.File.CreateText(Server.MapPath(String.Concat(FilePath, "xml/", fileConfigName)))
+        configFile.Write(file.FileName)
+        configFile.Close()
+
+    End Sub
+
+    Private Class FileInfoCompare
+        Implements IComparer(Of FileInfo)
+
+        Public Function Compare(ByVal x As System.IO.FileInfo, ByVal y As System.IO.FileInfo) As Integer Implements System.Collections.Generic.IComparer(Of System.IO.FileInfo).Compare
+            Return x.CreationTime > y.CreationTime
+        End Function
+    End Class
+
+    Private Sub WriteFileList()
+        Dim FileNameResult As System.Text.StringBuilder, FileNameFormat As String = "<tr><td style=""min-width:360px;_width:360px;_overflow-x:visible""><a title=""{1}"" href=""Destination.aspx?get={0}"" target=""_blank"">{1}</a></td><td style=""width:160px"">{2}</td><td style=""width:80px""><a href=""Destination.aspx?del={0}"">Delete</a></td></tr>"
+        FileNameResult = New System.Text.StringBuilder()
+
+        Dim rootPath As DirectoryInfo = New DirectoryInfo(Server.MapPath(FilePath))
+        Dim allFiles As FileInfo() = rootPath.GetFiles("*", SearchOption.TopDirectoryOnly)
+
+        If allFiles.Length > 0 Then
+            Dim files As List(Of FileInfo) = New List(Of FileInfo)(allFiles)
+
+            files.Sort(New FileInfoCompare())
+
+            For Each file As FileInfo In files
+
+                Dim fileRealName As String = GetFileRealFile(file.Name)
+                FileNameResult.Append(String.Format(FileNameFormat, file.Name, fileRealName, file.CreationTime.ToString("yyyy-MM-dd HH:mm:ss")))
+
+            Next
+
+            FileListStr = FileNameResult.ToString()
+        Else
+            FileListStr = "<tr><td style=""color:#888""><i>Nothing yet</i></td></tr>"
+        End If
+
+    End Sub
+
+    Private Sub DeleteFile(ByVal fileName As String)
+        Dim orgFileName As String = Server.MapPath(String.Concat(FilePath, fileName))
+
+        Dim configFileName As String = Server.MapPath(String.Concat(FilePath, "xml/", fileName, ".config"))
+
+        If File.Exists(orgFileName) Then
+            Try
+                File.Delete(orgFileName)
+                File.Delete(configFileName)
+            Catch
+            End Try
+        End If
+    End Sub
+
+    Private Sub DownloadFile(ByVal fileName As String)
+        Dim realFileName As String = GetFileRealFile(fileName)
+        Response.Clear()
+        Response.AddHeader("Content-Type", "Application/Octect-Stream")
+        Dim fileNameHeader As String = "attachment;filename={0}"
+
+        Dim userAgent As String = Request.UserAgent.ToLower()
+        Dim isIE As Boolean = (userAgent.IndexOf("msie") > -1)
+        Dim isFirefox As Boolean = (userAgent.IndexOf("firefox") > -1)
+
+        Dim fileNameContent As String = realFileName
+        If (isIE) Then
+            fileNameContent = Uri.EscapeUriString(realFileName)
+        ElseIf (isFirefox) Then
+            fileNameContent = String.Concat("""", realFileName, """")
+        End If
+
+        fileNameHeader = String.Format(fileNameHeader, fileNameContent)
+
+        Response.AddHeader("Content-Disposition", fileNameHeader)
+
+        Response.WriteFile(Server.MapPath(String.Concat(FilePath, fileName)))
+        Response.End()
+    End Sub
+
+    Private Function GetFileRealFile(ByVal fileName As String) As String
+        Dim fnIndex As String = fileName.LastIndexOf(Path.DirectorySeparatorChar)
+        If fnIndex = -1 Then
+            fnIndex = 0
+        End If
+
+        Dim guidName As String = fileName.Substring(fnIndex)
+
+        Dim fileRealName As String = File.ReadAllText(Server.MapPath(String.Concat(FilePath, "/xml/", guidName, ".config")))
+
+        Return fileRealName
+    End Function
+
+End Class
+
+
